4806817e64fd9e03affebea019c75f011e0580a3
Chi Le <cdle@cpp.edu>
2023-10-22 19:46:22 -0700

    Completed createDocument, deleteDocument, updateDocument

--- db_connection_solution.py
+++ db_connection_solution.py
@@ -3,7 +3,7 @@
 # FILENAME: title of the source file
 # SPECIFICATION: description of the program
 # FOR: CS 4250- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 60min
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with
@@ -14,6 +14,7 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import re
+import string
 
 def connectDataBase():
 
@@ -65,12 +66,52 @@ def createDocument(cur, docId, docText, docTitle, docDate, docCat):
     # 3.2 For each term identified, check if the term already exists in the database
     # 3.3 In case the term does not exist, insert it into the database
     # --> add your Python code here
+    indexList = {}
+    termList = {}
+    newTermList = {}
+    # Split docText into terms, remove punctuation
+    translator = str.maketrans('', '', string.punctuation)
+    termInDoc = [term.translate(translator).lower() for term in docText.split() if term]
+
+    # Retrieve all terms
+    cur.execute("SELECT * FROM term")
+    result = cur.fetchall()
+    for row in result:
+        term = row['term']
+        num_chars = row['num_chars']
+        termList[term] = num_chars
+        newTermList[term] = num_chars
+
+    # Update indexList
+    for term in termInDoc:
+        if term not in termList:
+            newTermList[term] = len(term)
+
+    # Update term database
+    for term, num_chars in newTermList.items():
+        if term not in termList:
+            sql = "Insert into term(term, num_chars) Values (%s, %s)"
+            recset = [term, num_chars]
+            cur.execute(sql, recset)
 
     # 4 Update the index
     # 4.1 Find all terms that belong to the document
     # 4.2 Create a data structure the stores how many times (count) each term appears in the document
     # 4.3 Insert the term and its corresponding count into the database
     # --> add your Python code here
+
+    # Update indexList
+    for term in termInDoc:
+        if term in indexList:
+            indexList[term] += 1
+        else:
+            indexList[term] = 1
+
+    # Update index database
+    for term, count in indexList.items():
+        sql = "Insert into index(doc_id, term, count) Values (%s, %s, %s)"
+        recset = [docId, term, count]
+        cur.execute(sql, recset)
     print("Document created.")
 
 def deleteDocument(cur, docId):
@@ -79,19 +120,34 @@ def deleteDocument(cur, docId):
     # 1.1 For each term identified, delete its occurrences in the index for that document
     # 1.2 Check if there are no more occurrences of the term in another document. If this happens, delete the term from the database.
     # --> add your Python code here
+    # Retrieve all terms
+    cur.execute("SELECT term FROM index WHERE doc_id = %s", (docId,))
+    termInDoc = cur.fetchall()
+    for row in termInDoc:
+        term = row['term']
+        sql = "Delete from index where doc_id = %(doc_id)s AND term = %(term)s"
+        cur.execute(sql, {'doc_id': docId, 'term': term})
+        cur.execute("SELECT term FROM index WHERE term = %s", (term,))
+        termInOtherDoc = cur.fetchall()
+        if len(termInOtherDoc) == 0:
+            sql = "Delete from term where term = %(term)s"
+            cur.execute(sql, {'term': term})
 
     # 2 Delete the document from the database
     # --> add your Python code here
+    sql = "Delete from document where doc_id = %(doc_id)s"
+    cur.execute(sql, {'doc_id': docId})
     print("Document deleted.")
 
 def updateDocument(cur, docId, docText, docTitle, docDate, docCat):
 
     # 1 Delete the document
     # --> add your Python code here
+    deleteDocument(cur, docId)
 
     # 2 Create the document with the same id
     # --> add your Python code here
-
+    createDocument(cur, docId, docText, docTitle, docDate, docCat)
     print("Document updated.")
 def getIndex(cur):
 
@@ -99,4 +155,3 @@ def getIndex(cur):
     # {'baseball':'Exercise:1','summer':'Exercise:1,California:1,Arizona:1','months':'Exercise:1,Discovery:3'}
     # ...
     # --> add your Python code here
-    print("Index is: ")
